b43cd2c8150fd795f8bc94c463d7fa1dc7b052c4
Felipe Antero <souzantero@gmail.com>
2024-12-04 16:52:08 -0300

    feat: generate transaction id

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-pub type Id = i32;
-pub type Timestamp = i64;
+use std::time::SystemTime;
+
+const MAX_TRANSACTIONS_PER_BLOCK: usize = 5;
+
+pub type Id = u32;
+pub type Timestamp = u64;
 pub type AccountId = String;
 pub type Hash = String;
 pub type Amount = f64;
 
+#[derive(Debug)]
 pub struct Transaction {
     pub id: Id,
     pub origin: AccountId,
@@ -11,6 +16,7 @@ pub struct Transaction {
     pub amount: Amount,
 }
 
+#[derive(Debug)]
 pub struct Block {
     pub id: Id,
     pub timestamp: Timestamp,
@@ -21,9 +27,13 @@ pub struct Block {
 
 impl Block {
     pub fn new(id: Id, previous_hash: Hash) -> Self {
-        let timestamp = 0;
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let hash = format!("{:?}", (id, &previous_hash, timestamp));
         let transactions: Vec<Transaction> = Vec::new();
-        let hash = "".to_string();
         Block {
             id,
             timestamp,
@@ -34,6 +44,7 @@ impl Block {
     }
 }
 
+#[derive(Debug)]
 pub struct Blockchain {
     pub blocks: Vec<Block>,
 }
@@ -44,20 +55,26 @@ impl Blockchain {
     }
 
     pub fn create_transaction(&mut self, origin: &str, destination: &str, amount: Amount) {
+        let current_block = self.get_current_block();
+        let last_transaction = current_block.transactions.last();
+        let id = match last_transaction {
+            Some(transaction) => transaction.id + 1,
+            None => 1,
+        };
+
         let transaction = Transaction {
-            id: 1,
+            id,
             origin: origin.to_string(),
             destination: destination.to_string(),
             amount,
         };
 
-        let current_block = self.get_current_block();
         current_block.transactions.push(transaction);
     }
 
     fn get_current_block(&mut self) -> &mut Block {
         if let Some(current_block) = self.blocks.last_mut() {
-            if current_block.transactions.len() >= 5 {
+            if current_block.transactions.len() >= MAX_TRANSACTIONS_PER_BLOCK {
                 let new_block = Block::new(current_block.id + 1, current_block.hash.clone());
                 self.blocks.push(new_block);
             }
@@ -73,4 +90,10 @@ impl Blockchain {
 fn main() {
     let mut blockchain = Blockchain::new();
     blockchain.create_transaction("Alice", "Bob", 100.0);
+    blockchain.create_transaction("Bob", "Alice", 50.0);
+    blockchain.create_transaction("Alice", "Bob", 25.0);
+    blockchain.create_transaction("Alice", "Bob", 10.0);
+    blockchain.create_transaction("Bob", "Alice", 5.0);
+    blockchain.create_transaction("Alice", "Bob", 2.5);
+    println!("{:?}", blockchain);
 }
